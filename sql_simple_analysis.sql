91b52aa3d17f1c837b96aa6ef9f715b1f84750dc
Denis <nizkochastotnifiltr@gmail.com>
2022-01-10 18:56:53 +0500

    simlpe_reading

--- sql_simple_analysis.sql
+++
@@ -1,4 +0,0 @@
-use classicmodels;
-
-select *
-from customers
---
+++ sql_simple_read.sql
@@ -0,0 +1,25 @@
+use classicmodels;
+
+select *
+from customers;
+
+select *
+from employees;
+
+select *
+from offices;
+
+select *
+from orderdetails;
+
+select *
+from orders;
+
+select *
+from payments;
+
+select *
+from products;
+
+select *
+from productlines;
