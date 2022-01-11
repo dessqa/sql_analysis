9f0f513b3ec7b518bb3a8b28240ddfbb886a4afe
Denis <nizkochastotnifiltr@gmail.com>
2022-01-11 17:47:29 +0500

    Update sql_analysis_total_sales.sql

--- sql_analysis_total_sales.sql
+++ sql_analysis_total_sales.sql
@@ -9,7 +9,7 @@ select *
 from customers;
 
 select *
-from orderdetails;
+from orderdetails;products
 
 # Посчитаем общее число заказов
 select count(orders.orderNumber)
@@ -18,10 +18,10 @@ from orders;
 # Посчитаем общую прибыль с каждого заказа.
 # Потом объединим с таблицей Orders, чтобы вывести статус заказа.
 select orderdetails.orderNumber,
-		orderdetails.quantityOrdered,
-        orderdetails.priceEach,
-		(orderdetails.quantityOrdered * orderdetails.priceEach) AS totalPrice,
-        orders.status
+	orderdetails.quantityOrdered,
+	orderdetails.priceEach,
+	(orderdetails.quantityOrdered * orderdetails.priceEach) AS totalPrice,
+	orders.status
 from orderdetails
 left join orders
 on orderdetails.orderNumber = orders.orderNumber;
@@ -37,8 +37,7 @@ order by sum(orderdetails.quantityOrdered * orderdetails.priceEach) desc;
 
 # Если учесть, что полученные цифры по данному запросу это потенциальная прибыль магизина,
 # можно посчитать процент потерь по каждому статусу
-select 
-	sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Потенциальная прибыль компании"
+select sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Потенциальная прибыль компании"
 from orderdetails
 left join orders
 on orderdetails.orderNumber = orders.orderNumber;
@@ -46,7 +45,7 @@ on orderdetails.orderNumber = orders.orderNumber;
 # Теперь можем посчитать процент прибыли/потерь
 select orders.status as "Статус",
 	sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Денежный оборот", #Считаем сумму заказов по статусам
-    ((sum(orderdetails.quantityOrdered * orderdetails.priceEach) / (
+	((sum(orderdetails.quantityOrdered * orderdetails.priceEach) / (
     	select sum(quantityOrdered * priceEach) #Считаем всю прибыль (с отказами, холдами и  прочим)
 		from orderdetails)) * 100) as "Процентное соотношение прибыли/затрат на общую потенциальную прибыль"
 from orderdetails
@@ -54,3 +53,14 @@ left join orders
 on orderdetails.orderNumber = orders.orderNumber
 group by orders.status
 order by sum(orderdetails.quantityOrdered * orderdetails.priceEach) desc;
+
+# Теперь считаем объем продаж по кол-ву купленных товаров
+select 
+	orderdetails.productCode as "Код товара",
+	orders.status,
+	sum(orderdetails.quantityOrdered) as "Сумма товаров"
+from orderdetails
+left join orders
+on orderdetails.orderNumber = orders.orderNumber
+group by orderdetails.productCode, orders.status
+order by orderdetails.productCode, orders.status desc
