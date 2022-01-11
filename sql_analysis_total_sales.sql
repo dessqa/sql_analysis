7f8231fd9d7d4b087e4a75961969be297978b4fc
Denis <nizkochastotnifiltr@gmail.com>
2022-01-11 14:33:49 +0500

    Update sql_analysis_total_sales.sql

--- sql_analysis_total_sales.sql
+++ sql_analysis_total_sales.sql
@@ -27,10 +27,30 @@ left join orders
 on orderdetails.orderNumber = orders.orderNumber;
 
 # Далее посчитаем прибыль/потери по всем статусам
-select sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS totalPrice,
-        orders.status
+select orders.status as "Статус",
+	sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Денежный оборот"
 from orderdetails
 left join orders
 on orderdetails.orderNumber = orders.orderNumber
 group by orders.status
+order by sum(orderdetails.quantityOrdered * orderdetails.priceEach) desc;
 
+# Если учесть, что полученные цифры по данному запросу это потенциальная прибыль магизина,
+# можно посчитать процент потерь по каждому статусу
+select 
+	sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Потенциальная прибыль компании"
+from orderdetails
+left join orders
+on orderdetails.orderNumber = orders.orderNumber;
+
+# Теперь можем посчитать процент прибыли/потерь
+select orders.status as "Статус",
+	sum(orderdetails.quantityOrdered * orderdetails.priceEach) AS "Денежный оборот", #Считаем сумму заказов по статусам
+    ((sum(orderdetails.quantityOrdered * orderdetails.priceEach) / (
+    	select sum(quantityOrdered * priceEach) #Считаем всю прибыль (с отказами, холдами и  прочим)
+		from orderdetails)) * 100) as "Процентное соотношение прибыли/затрат на общую потенциальную прибыль"
+from orderdetails
+left join orders
+on orderdetails.orderNumber = orders.orderNumber
+group by orders.status
+order by sum(orderdetails.quantityOrdered * orderdetails.priceEach) desc;
